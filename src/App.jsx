579e60af2f3a78728981c88368d12aeab9bfc284
rabby6530 <you@example.com>
2023-03-27 12:31:13 +0600

    added product

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './component/Header/Header'
+import Shop from './component/Shop/Shop'
 
 function App() {
   return (
     <div className="App">
      <Header></Header>
+     <Shop></Shop>
     </div>
   )
 }
---
+++ src/component/Product/Product.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+    console.log(props.product)
+    const { name, img, price, ratings, quantity, seller } = props.product;
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price:${price}</p>
+                <p>Manufacturer:{seller}</p>
+                <p>Rating:{ratings} Stars</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Product;
